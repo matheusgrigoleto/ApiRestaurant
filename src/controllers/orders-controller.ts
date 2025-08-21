35404041ad53b9ed238f2f1366576d6772f029b7
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-21 18:50:28 -0300

    FEAT: Listing orders per tables

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -48,6 +48,27 @@ class OrdersController {
       next(error);
     }
   }
+
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      const { table_session_id } = request.params;
+
+      const order = await knex("orders")
+        .select(
+          "orders.id",
+          "orders.table_session_id",
+          "orders.product_id",
+          "products.name",
+          "orders.price",
+          "orders.quantity"
+        )
+        .join("products", "products.id", "orders.product_id")
+        .where({ table_session_id });
+      return response.json(order);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 export { OrdersController };
--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -4,6 +4,7 @@ import { OrdersController } from "@/controllers/orders-controller";
 const ordersRouters = Router();
 const ordersController = new OrdersController();
 
+ordersRouters.get("/table-session/:table_session_id", ordersController.index)
 ordersRouters.post("/", ordersController.create);
 
 export { ordersRouters };
