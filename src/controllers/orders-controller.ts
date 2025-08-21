c09f6fe3bcfd0a69f350f0feffbb2bb14e30c5bb
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-21 18:33:10 -0300

    FEAT: Order business rules

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,6 +1,7 @@
-import { table } from "console";
+import { knex } from "@/database/knex";
 import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
+import { AppError } from "@/utils/AppError";
 
 class OrdersController {
   async create(request: Request, response: Response, next: NextFunction) {
@@ -15,7 +16,27 @@ class OrdersController {
         request.body
       );
 
-      return response.status(201).json();
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        .where({ id: table_session_id })
+        .first();
+
+      if (!session) {
+        throw new AppError("session table not found");
+      }
+
+      if (session.closed_at) {
+        throw new AppError("this table is closed");
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first();
+
+      if (!product) {
+        throw new AppError("product not found");
+      }
+
+      return response.status(201).json(product);
     } catch (error) {
       next(error);
     }
