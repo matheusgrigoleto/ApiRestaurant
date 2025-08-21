6a7a00c8de52b1ad6d9f3456c41ea29ad25dde79
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-21 16:42:40 -0300

    FEAT: Added open table

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,8 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod"
+import { knex } from "@/database/knex";
 
 class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
+      const bodySchema = z.object({
+        table_id: z.number()
+      })
+
+      const { table_id } = bodySchema.parse(request.body)
+
+      await knex<TablesSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      })
+
       return response.status(201).json()
     } catch (error) {
       next(error)
