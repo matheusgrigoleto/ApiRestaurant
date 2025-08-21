50c406d6ed14283bae85247b5fac8c00c257f55e
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-21 16:28:03 -0300

    FEAT: Created route and controller to tables sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { productsRoutes } from "./products-routes";
 import { tablesRouters } from "./tables-routes";
+import { tablesSessionsRoutes } from "./tables-sessions-routes";
 
 const routes = Router();
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRouters);
+routes.use("/tables-sessions", tablesSessionsRoutes)
 
 export { routes };
