a509d89d893ad4abe864cbce9b9932b5bf28ad3d
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-21 19:14:21 -0300

    FEAT: Calculating the total bill

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -6,5 +6,6 @@ const ordersController = new OrdersController();
 
 ordersRouters.get("/table-session/:table_session_id", ordersController.index)
 ordersRouters.post("/", ordersController.create);
+ordersRouters.get("/table-session/:table_session_id/total", ordersController.show);
 
 export { ordersRouters };
